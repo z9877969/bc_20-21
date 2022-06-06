3e33e3b3a7a8652b5c8d34ea31b0860717729e34
z9877969 <z9877969@ukr.net>
2022-06-06 13:29:36 +0300

    adds variables & data-types

---
+++ lesson-01(variable&data_types)/js/index.js
@@ -0,0 +1,147 @@
+"use strict";
+// JS чутливий до регістру
+var year = 2018;
+let month = "November my amazimg month &&& 654654 ???";
+const buleanA = true;
+const buleanB = false;
+const n = null;
+const und = undefined;
+const obj = {};
+const fn = function () {};
+const id = Symbol(5);
+
+// var value = 5;
+const monthA = "November";
+let car = "BMW";
+
+// monthA = 25;
+// console.log(monthA);
+// console.log(car);
+// car = "AUDI";
+// console.log(car);
+// // console.log("console", "qweqwe", " ", "car-", car);
+// const a = null;
+// let someNum;
+// console.log(someNum);
+// someNum = 25;
+// console.log(someNum);
+
+// const userFirstName = "Bob";
+// const _a = "a";
+// const $b = "b";
+// const User = "user";
+// const user = "user";
+// const API_KEY = "qweqwe654qweqwA554654";
+
+// const a = 5 + 2; console.log("object"); let b; b = 54;
+// const a = 5 + 2;
+// console.log("object");
+// let b;
+// b = 54;
+
+// const userInput = prompt("Input value");
+// console.log(userInput);
+// alert(userInput);
+// const conf = confirm("YEs or No");
+// console.log(conf);
+
+// const str = "";
+// const num = 0;
+// const bul = true;
+
+// console.log(typeof str);
+// console.log(typeof num);
+// console.log(typeof null);
+// console.log(typeof 654);
+
+// числа та основні оператори: +, -, *, /, %, **
+// const firstNum = 2;
+// const secondNum = 3;
+// console.log(firstNum + secondNum);
+// console.log(firstNum - secondNum);
+// const result = firstNum * secondNum;
+// console.log(result);
+// const result = firstNum / secondNum;
+// console.log(result);
+// const result = firstNum % secondNum;
+// console.log(result);
+
+// const even = 4 % 2;
+// const odd = 5 % 2;
+
+// console.log(even);
+// console.log(odd);
+// const result = 2 ** 3;
+// console.log("result :>> ", result);
+
+// const str = "21";
+// const newNum = Number(str);
+// console.log("str :>> ", typeof str);
+// console.log("newNum :>> ", typeof newNum);
+
+// const str = "21";
+// const newNum = +str
+// console.log("newNum :>> ", newNum);
+
+// const str = "qwe";
+// const newNum = +str;
+// console.log("newNum :>> ", newNum);
+// console.log("newNum :>> ", typeof newNum);
+
+// const inputNumber = prompt("Input number");
+// const result = Number(inputNumber) + 25;
+// console.log("result :>> ", result);
+
+// console.log(Number(true)); // 1
+// console.log(Number(false)); // 0
+// console.log(Number(null)); // 0
+// console.log(Number("string"));
+// console.log(Number("")); // 0
+// console.log(Number(undefined));
+
+// const a = 0.1 + 0.2;
+// const aFixed = +a.toFixed(2);
+// console.log(typeof aFixed);
+
+// const a = "21px";
+// const pxNum = parseInt(a);
+// console.log("pxNum :>> ", pxNum);
+
+// const a = "21.5px";
+// const pxNum = parseFloat(a);
+// console.log("pxNum :>> ", pxNum);
+
+// Math (float, ceil, round, min, max)
+// const a = 21.5;
+// const minA = Math.floor(a);
+// console.log("minA :>> ", minA);
+
+// const b = 15.4;
+// const maxB = Math.ceil(b);
+// console.log("maxB :>> ", maxB);
+
+// const c = 10.6;
+// const mathC = Math.round(c);
+// console.log("mathC :>> ", mathC);
+
+// console.log(Math.min(5, 6, 2, 10, 0));
+// console.log(Math.max(5, 6, 2, 10, 0));
+
+// const randomNum = Math.random();
+// console.log("randomNum :>> ", randomNum);
+// const createNum = Math.random() * 100;
+// const parseNum = Math.round(createNum);
+// console.log("parseNum :>> ", parseNum);
+
+// const str = "654lkjlk";
+// const canNum = isNaN(str); // "654lkjlk" -> true | "654" -> false
+// console.log("canNum :>> ", canNum);
+// const num = Number("jhgjhg");
+
+// console.log(isNaN("true"));
+
+// let a = 10;
+// let b = Infinity;
+// console.log(a / 0);
+
+
