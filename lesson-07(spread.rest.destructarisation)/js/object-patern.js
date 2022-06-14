35cf3bf23aff7e1bc232f461bd9da679309cefeb
z9877969 <z9877969@ukr.net>
2022-06-14 13:10:13 +0300

    adds spread, rest, destructarisation

---
+++ lesson-07(spread.rest.destructarisation)/js/object-patern.js
@@ -0,0 +1,34 @@
+"use strict";
+
+// const logFunction = function (obj) {
+//     // 2
+//   const { firstName, isActive, age, lastName, userEmail, gender } = obj;
+
+// //   3
+//   console.log("firstName :>> ", firstName);
+// //   4
+//   console.log("lastName :>> ", lastName);
+// //   5
+//   console.log("age :>> ", age);
+//   console.log("gender :>> ", gender);
+//   console.log("userEmail :>> ", userEmail);
+//   console.log("isActive :>> ", isActive);
+// };
+
+// logFunction({
+//   firstName: "Bart",
+//   lastName: "Simpson",
+//   age: 20,
+//   gender: "mail",
+//   userEmail: "bart@mail.com",
+//   isActive: false,
+// });
+
+// logFunction({
+//   age: 24,
+//   gender: "mail",
+//   userEmail: "bill@mail.com",
+//   lastName: "Rock",
+//   isActive: false,
+//   firstName: "Bill",
+// });
