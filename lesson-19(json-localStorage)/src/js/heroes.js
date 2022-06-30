cac16ce7a90629fa0c4d604b658cdd44c2abff5e
z9877969 <z9877969@ukr.net>
2022-06-30 13:37:22 +0300

    adds json, localStorage

---
+++ lesson-19(json-localStorage)/src/js/heroes.js
@@ -0,0 +1,17 @@
+'use strict';
+// import heroesDb from './heroes.json';
+
+/*
+<li class="heroes__item">
+    <img src="${url}" alt="${name}" class="heroes__picture">
+    <div class="heroes__content">
+        <h2 class="heroes__name">${name}</h2>
+        <ul class="heroes__biography list">
+            <li class="heroes__biography-item"><p class="heroes__gender"><strong>Gender: </strong> ${gender}</p></li>
+            <li class="heroes__biography-item"><p class="heroes__alter-egos"><strong>Alter Egos: </strong> ${alterEgos}</p></li>
+        </ul>
+    </div>
+</li>
+*/
+
+const heroesListEl = document.querySelector('.js-heroes-list');
---
+++ lesson-19(json-localStorage)/src/js/math.js
@@ -0,0 +1,27 @@
+'use strict';
+
+export const add = (a, b) => {
+  return a + b;
+};
+
+export const sub = (a, b) => {
+  return a - b;
+};
+
+export const division = (a, b) => {
+  return a / b;
+};
+
+export const multi = (a, b) => {
+  return a * b;
+};
+
+// const mathFns = {
+//   add,
+//   sub,
+//   division,
+// };
+
+// console.log('object');
+
+// export default mathFns;
