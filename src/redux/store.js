db6bd581611b03cdbc4249bed69ab66c5fe09218
z9877969 <z9877969@ukr.net>
2022-08-11 09:48:05 +0300

    update store

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,32 +2,10 @@ import { createStore, combineReducers } from "redux";
 import { composeWithDevTools } from "@redux-devtools/extension";
 import counterReducer from "./counter/counterReducer";
 import todo from "./todo/todoReducer";
-import { actionTypes } from "./counter/counterActions";
-
-const changeMessageAction = (value) => ({
-  type: "change_message",
-  payload: value,
-});
-
-changeMessageAction("qwe");
-
-const isOpenReducer = (state = false, action) => state;
-const messageReducer = (state = "", action) => {
-  switch (action.type) {
-    case "change_message":
-      return action.payload;
-    case actionTypes.COUNTER_RESET:
-      return "action payload is " + action.payload;
-    default:
-      return state;
-  }
-};
 
 const rootReducer = combineReducers({
   counter: counterReducer,
   todo,
-  isOpen: isOpenReducer, // isOpenReducer(false, changeMessageAction)
-  message: messageReducer, // messageReducer("", changeMessageAction)
 });
 
 // () => {}
